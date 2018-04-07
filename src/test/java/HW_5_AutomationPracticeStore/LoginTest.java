f54d2fa92b58b6b2a30dd0cac13bfd77cdf88190
OleksiiKHM <piromanbf2@gmail.com>
2018-04-07 14:42:24 +0300

    refactoring

--- src/test/java/HW_5_AutomationPracticeStore/LoginTest.java
+++ src/test/java/HW_5_AutomationPracticeStore/LoginTest.java
@@ -1,17 +1,23 @@
-package AutomationPracticeStore;
+package HW_5_AutomationPracticeStore;
 
 import org.junit.*;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LoginTest {
 
     static WebDriver myDriver = new ChromeDriver();
     String userEmail = "hillbilly1969@yahoo.com";
     String userPasswd = "Amuricastrong";
+
+    public static void getBackToMyAccount() {
+        myDriver.navigate().back();
+        Assert.assertEquals(myDriver.getTitle(), "My account - My Store");
+    }
 
     @BeforeClass
     public static void setUp() {
@@ -25,7 +31,7 @@ public class LoginTest {
     }
 
     @Test
-    public void loginTest() {
+    public void _1_loginTest() {
         myDriver.findElement(By.xpath("//a[@class='login']")).click();
         Assert.assertEquals(myDriver.getTitle(), "Login - My Store");
 
@@ -36,41 +42,42 @@ public class LoginTest {
     }
 
     @Test
-    public void myAccountLinksTest() {
-        Assert.assertEquals(myDriver.getTitle(), "My account - My Store");
-
-        myDriver.findElement(By.xpath("//a[@title='Orders']")).click();
-        Assert.assertEquals(myDriver.getTitle(), "Order history - My Store");
-        Assert.assertEquals(myDriver.getCurrentUrl(), "http://automationpractice.com/index.php?controller=history");
-
-        myDriver.navigate().back();
-        Assert.assertEquals(myDriver.getTitle(), "My account - My Store");
-
-        myDriver.findElement(By.xpath("//a[@title='Credit slips']")).click();
-        Assert.assertEquals(myDriver.getTitle(), "Order slip - My Store");
-        Assert.assertEquals(myDriver.getCurrentUrl(), "http://automationpractice.com/index.php?controller=order-slip");
-
-        myDriver.navigate().back();
-        Assert.assertEquals(myDriver.getTitle(), "My account - My Store");
-
-        myDriver.findElement(By.xpath("//a[@title='Addresses']")).click();
-        Assert.assertEquals(myDriver.getTitle(), "Addresses - My Store");
-        Assert.assertEquals(myDriver.getCurrentUrl(), "http://automationpractice.com/index.php?controller=addresses");
-
-        myDriver.navigate().back();
-        Assert.assertEquals(myDriver.getTitle(), "My account - My Store");
-
+    public void _2_openPersonalInfoTest() {
         myDriver.findElement(By.xpath("//a[@title='Information']")).click();
         Assert.assertEquals(myDriver.getTitle(), "Identity - My Store");
         Assert.assertEquals(myDriver.getCurrentUrl(), "http://automationpractice.com/index.php?controller=identity");
+        getBackToMyAccount();
+    }
 
-        myDriver.navigate().back();
+    @Test
+    public void _3_openAddressesTest() {
+        myDriver.findElement(By.xpath("//a[@title='Addresses']")).click();
+        Assert.assertEquals(myDriver.getTitle(), "Addresses - My Store");
+        Assert.assertEquals(myDriver.getCurrentUrl(), "http://automationpractice.com/index.php?controller=addresses");
+        getBackToMyAccount();
+    }
+
+    @Test
+    public void _4_openCreditSlipsTest() {
+        myDriver.findElement(By.xpath("//a[@title='Credit slips']")).click();
+        Assert.assertEquals(myDriver.getTitle(), "Order slip - My Store");
+        Assert.assertEquals(myDriver.getCurrentUrl(), "http://automationpractice.com/index.php?controller=order-slip");
+        getBackToMyAccount();
+    }
+
+    @Test
+    public void _5_openOrderHistoryTest() {
         Assert.assertEquals(myDriver.getTitle(), "My account - My Store");
+        myDriver.findElement(By.xpath("//a[@title='Orders']")).click();
+        Assert.assertEquals(myDriver.getTitle(), "Order history - My Store");
+        Assert.assertEquals(myDriver.getCurrentUrl(), "http://automationpractice.com/index.php?controller=history");
+        getBackToMyAccount();
+    }
 
+    @Test
+    public void _6_openWishlistTest() {
         myDriver.findElement(By.xpath("//a[@title='My wishlists']")).click();
         Assert.assertEquals(myDriver.getTitle(), "My Store");
         Assert.assertEquals(myDriver.getCurrentUrl(), "http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist");
-
     }
-
 }
